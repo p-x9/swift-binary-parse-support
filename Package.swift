ba04219729f0c1d543fd5f4f7e005a5c7c86c6c2
p-x9 <50244599+p-x9@users.noreply.github.com>
2026-01-05 22:38:17 +0900

    Update package name

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "BinaryParseSupport",
+    name: "swift-binary-parse-support",
     products: [
         .library(
             name: "BinaryParseSupport",
